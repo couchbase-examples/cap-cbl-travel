aa89de7b0c612406fc82b8060d7bddf6c9635823
Aaron LaBeau <80424345+biozal@users.noreply.github.com>
2024-11-14 07:08:26 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cap-cbl-travel-hotels',
+  webDir: 'dist'
+};
+
+export default config;
